6f6a6def78c5e02d54bd6d1ebfbca80677879a89
Simonesiss <simona.radulea@gmail.com>
2021-01-31 23:24:54 +0200

    Create styles file
    Added colors for light theme

---
+++ helpers/style.js
@@ -0,0 +1,37 @@
+export const colors = {
+    light: {
+        backgroundColor: "#FFFFFF",
+        textColor: "#323232",
+        // headerTextColor: "#FFFFFF",
+        button: "#6B75CE", // Dark Blue 2
+        googleButton: "#DB4437",
+        facebookButton: "#3B5998",
+        linkBlue: "#564B93", // Dark Blue
+        textGray: "#B4C5D2", // Gray
+        lightViolet: "#D4C3FC", // Light Violet
+        red: "#F6707B",
+        green: "#6ED687",
+        cardBlue: "#5C8DF7", // Blue
+        cardLightViolet: "#D4C3F33F", // Light Violet 25%
+        cardLightBlue: "#A5C5FC3F", // Light Blue 25%
+        violet: "#9B8CF8", // Violet
+    },
+    // to be completed
+    dark: {
+        backgroundColor: "",
+        textColor: "",
+        // headerTextColor: "",
+        button: "", // Dark Blue 2
+        googleButton: "",
+        facebookButton: "",
+        linkBlue: "", // Dark Blue
+        textGray: "", // Gray
+        lightViolet: "", // Light Violet
+        red: "",
+        green: "",
+        cardBlue: "", // Blue
+        cardLightViolet: "", // Light Violet 25%
+        cardLightBlue: "", // Light Blue 25%
+        violet: "", // Violet
+    }
+}
