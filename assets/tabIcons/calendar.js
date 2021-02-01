b89f306ab9e4e97649e413bbe8639e2136b04fa5
Simonesiss <simona.radulea@gmail.com>
2021-02-01 20:21:13 +0200

    Add icon SVG

---
+++ assets/tabIcons/calendar.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+export default (props) => {
+    return (
+        <Svg
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <Path
+                d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V10h16v11zm0-13H4V5h16v3z"
+                fill="#323232"
+            />
+        </Svg>
+    )
+}
