608928f568577fa17fe5e82e5a058bad02103df9
Simonesiss <simona.radulea@gmail.com>
2021-02-26 16:08:23 +0200

    Update background

--- assets/backgrounds/background.js
+++ assets/backgrounds/background.js
@@ -1,8 +1,9 @@
 import * as React from "react"
+import Svg, { Path, Defs, LinearGradient, Stop } from "react-native-svg"
 
 export default (props) => {
     return (
-        <svg
+        <Svg
             width={375}
             height={812}
             viewBox="0 0 375 812"
@@ -10,13 +11,13 @@ export default (props) => {
             xmlns="http://www.w3.org/2000/svg"
             {...props}
         >
-            <path
+            <Path
                 fill="url(#prefix__paint0_linear)"
                 fillOpacity={0.25}
                 d="M0 0h375v812H0z"
             />
-            <defs>
-                <linearGradient
+            <Defs>
+                <LinearGradient
                     id="prefix__paint0_linear"
                     x1={-23.5}
                     y1={-25.621}
@@ -24,11 +25,11 @@ export default (props) => {
                     y2={624.044}
                     gradientUnits="userSpaceOnUse"
                 >
-                    <stop stopColor="#E8B7E5" />
-                    <stop offset={0.182} stopColor="#D4C3FC" />
-                    <stop offset={0.729} stopColor="#A5C5FC" />
-                </linearGradient>
-            </defs>
-        </svg>
+                    <Stop stopColor="#E8B7E5" />
+                    <Stop offset={0.182} stopColor="#D4C3FC" />
+                    <Stop offset={0.729} stopColor="#A5C5FC" />
+                </LinearGradient>
+            </Defs>
+        </Svg>
     )
 }
