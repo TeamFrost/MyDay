95bff02583c50167152cd4e2779793828bdd5aae
Simonesiss <simona.radulea@gmail.com>
2021-03-11 21:43:09 +0200

    Add Redux

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
+//Redux imports
+import { Provider } from 'react-redux'
+import { store } from './redux/store';
+
 //Navigation imports
 import { NavigationContainer } from '@react-navigation/native';
 import { LandingStackScreen } from './helpers/navigation'
 
 export default function App() {
   return (
-    <NavigationContainer>
-      {LandingStackScreen()}
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        {LandingStackScreen()}
+      </NavigationContainer>
+    </Provider>
   );
 }
 
