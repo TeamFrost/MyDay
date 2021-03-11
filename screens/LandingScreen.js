8462d314bfcd296ed9c2e86d9e2eb9af8d155c64
Simonesiss <simona.radulea@gmail.com>
2021-03-11 21:44:11 +0200

    Add Redux and Auth

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { connect } from 'react-redux';
 
 import Icon from '../assets/icon'
+import { restoreSession } from '../redux/actions/auth/auth';
+import { getData } from "../helpers/storage"
 
 import { colors } from '../helpers/style';
 
 const theme = colors.light;
 
-export default function LandingScreen() {
+const mapStateToProps = (state) => ({
+    doneFetching: state.auth.doneFetching,
+    isFetching: state.auth.isFetching,
+    hasError: state.auth.hasError,
+    errorMessage: state.auth.errorMessage,
+    user: state.auth.user,
+    theme: state.theme
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    restoreSession: () => dispatch(restoreSession()),
+    changeTheme: (theme) => dispatch(changeTheme(theme))
+});
+
+function LandingScreen({ ...props }) {
+
+    const { user, doneFetching, navigation, restoreSession, changeTheme } = props;
+
+    useEffect(() => {
+        getData('@reportm-theme')
+            .then((theme) => {
+                if (theme) {
+                    changeTheme(theme);
+                }
+                restoreSession()
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+
+    }, []);
+
+    if (doneFetching) {
+        // navigation.navigate('LoginStack')
+        setTimeout(() => {
+            if (user != null) {
+                navigation.navigate('HomeTabs')
+            }
+            else {
+                navigation.navigate('LoginStack')
+            }
+        }, 2000);
+    }
+
     return (
         <View style={styles.container}>
             <Icon height="250" width="250" />
@@ -31,3 +77,5 @@ const styles = StyleSheet.create({
         letterSpacing: -2.5,
     }
 });
+
+export default connect(mapStateToProps, mapDispatchToProps)(LandingScreen);
