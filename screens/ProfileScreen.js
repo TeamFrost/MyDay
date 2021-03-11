f83f852514dc1ce2600e9edb710584da6f1f24ce
Simonesiss <simona.radulea@gmail.com>
2021-03-11 21:44:22 +0200

    Add Logout

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,18 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { connect } from 'react-redux';
 
+import { logoutUser } from '../redux/actions/auth/auth';
 import { colors } from '../helpers/style';
 
 const theme = colors.light;
 
-export default function ProfileScreen() {
-    const navigation = useNavigation();
+const mapStateToProps = (state) => ({
+    doneFetching: state.auth.doneFetching,
+    isFetching: state.auth.isFetching,
+    hasError: state.auth.hasError,
+    errorMessage: state.auth.errorMessage,
+    user: state.auth.user,
+    theme: state.theme
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    logoutUser: () => dispatch(logoutUser()),
+});
+
+function ProfileScreen({ ...props }) {
+    const { user, doneFetching, navigation, logoutUser } = props
+
+
+    const handleLogoutPress = () => {
+        console.log("Logout")
+        logoutUser()
+    }
+
+    useEffect(() => {
+        if (doneFetching) {
+            console.log(user)
+            if (user === null) {
+                navigation.navigate('LoginStack')
+            }
+        }
+    }, []);
 
     return (
         <View style={styles.container}>
             <Text>Profile</Text>
+            <Button
+                onPress={handleLogoutPress}
+                title="Logout"
+                color="#841584"
+            />
             <StatusBar style="auto" />
         </View>
     );
@@ -26,3 +60,5 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileScreen);
