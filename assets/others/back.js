705df2cf10443f108df42d4126c2095133ccbecb
EdiOnetiu <edyonetiu@gmail.com>
2021-03-23 22:57:41 +0200

    Create back.js

---
+++ assets/others/back.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Svg, { Path } from "react-native-svg"
+
+export default (props) => {
+    return (
+        <Svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="43"
+            height="40"
+            fill="none"
+            viewBox="0 0 43 40"
+        >
+            <Path
+                fill="#fff"
+                d="M15.77 31.667l2.47-2.35-8.025-7.65h28.332v-3.334H10.215l8.042-7.65-2.488-2.35L3.504 20 15.77 31.667z">
+            </Path>
+        </Svg>
+    );
+}
+
