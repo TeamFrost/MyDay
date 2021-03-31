0ceac9fc76fa1ba4449f5c9c9fa423912cd1b0f7
Simonesiss <simona.radulea@gmail.com>
2021-03-31 17:54:52 +0300

    Add SVG

---
+++ assets/icons/logoutIcon.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+export default (props) => {
+    return (
+        <Svg
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <Path
+                d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5-5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"
+                fill="#F6707B"
+            />
+        </Svg>
+    )
+}
