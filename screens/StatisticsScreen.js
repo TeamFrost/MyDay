edd01c1c10b46266152399e112bb522b04526492
EdiOnetiu <edyonetiu@gmail.com>
2021-03-31 20:58:42 +0300

    Update StatisticsScreen.js

--- screens/StatisticsScreen.js
+++ screens/StatisticsScreen.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
 
+import HeaderGradient from '../assets/backgrounds/headerGradientBlue';
+import Back from '../assets/others/back.js';
 import { colors } from '../helpers/style';
 
+const screenWidth = Dimensions.get('screen').width;
+const screenHeight = Dimensions.get('screen').height;
 const theme = colors.light;
 
 const mapStateToProps = (state) => ({
@@ -18,7 +22,18 @@ function StatisticsScreen({ ...props }) {
 
     return (
         <View style={styles.container}>
-            <Text>Statistics</Text>
+            <HeaderGradient width={screenWidth * 1.2} height={"22%"} style={{ flex: 1, position: 'absolute' }} />
+
+            <View style={styles.topText}>
+                <TouchableWithoutFeedback onPress={() => navigation.goBack()}>
+                    <View style={styles.backButton}>
+                        <Back />
+                    </View>
+                </TouchableWithoutFeedback>
+                <Text style={styles.textTop}>Statistics</Text>
+            </View>
+
+
             <StatusBar style="auto" />
         </View>
     );
@@ -29,7 +44,23 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: theme.backgroundColor,
         alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    topText: {
+        width: '100%',
+        height: screenHeight / 4.8,
+        alignItems: 'center',
         justifyContent: 'center',
+    },
+    backButton: {
+        position: 'absolute',
+        top: '25%',
+        left: '5%'
+    },
+    textTop: {
+        color: "white",
+        fontSize: 26,
+        fontWeight: "bold"
     },
 });
 
