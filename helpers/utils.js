2fa6f2e6400412f63a347f001a62ea2c722b94ce
Simonesiss <simona.radulea@gmail.com>
2021-04-07 16:34:29 +0300

    Add static dimensions

---
+++ helpers/utils.js
@@ -0,0 +1,8 @@
+const IosScreenHeight = 844;
+const IosScreenWidth = 390;
+
+const AndroidScreenHeight = 914;
+const AndroidScreenWidth = 411;
+
+const EmulatorScreenHeight = 829;
+const EmulatorScreenWidth = 392;
