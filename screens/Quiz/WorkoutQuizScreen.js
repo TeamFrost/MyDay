43cdb64337daf8d56be018c57da83ef29859b907
EdiOnetiu <edyonetiu@gmail.com>
2021-04-08 15:31:14 +0300

    Update WorkoutQuizScreen.js

--- screens/Quiz/WorkoutQuizScreen.js
+++ screens/Quiz/WorkoutQuizScreen.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, TextInput, Dimensions, TouchableHighlight } from 'react-native';
 import { connect } from 'react-redux';
 
+import HeaderGradient from '../../assets/backgrounds/headerGradientBlue';
+import Back from '../../assets/others/back.js';
 import { colors } from '../../helpers/style';
 
 const theme = colors.light;
+const screenWidth = Dimensions.get('screen').width;
+const screenHeight = Dimensions.get('screen').height;
 
 const mapStateToProps = (state) => ({ theme: state.theme });
 
@@ -14,7 +18,17 @@ function WorkoutQuizScreen({ ...props }) {
 
     return (
         <View style={styles.container}>
-            <Text>Workout Quiz</Text>
+            <HeaderGradient width={screenWidth * 1.2} height={"22%"} style={{ flex: 1, position: 'absolute' }} />
+
+            <View style={styles.topText}>
+                <TouchableWithoutFeedback onPress={() => navigation.goBack()}>
+                    <View style={styles.backButton}>
+                        <Back />
+                    </View>
+                </TouchableWithoutFeedback>
+                <Text style={styles.textTop}>Workout Quiz</Text>
+            </View>
+
             <StatusBar style="auto" />
         </View>
     );
@@ -25,7 +39,24 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: theme.backgroundColor,
         alignItems: 'center',
+        justifyContent: 'flex-start',
+        width: '100%',
+    },
+    topText: {
+        width: '100%',
+        height: screenHeight / 4.8,
+        alignItems: 'center',
         justifyContent: 'center',
+    },
+    backButton: {
+        position: 'absolute',
+        top: '25%',
+        left: '5%'
+    },
+    textTop: {
+        color: "white",
+        fontSize: 26,
+        fontWeight: "bold"
     },
 });
 
