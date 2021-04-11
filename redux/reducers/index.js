aab5327dae150b58e9188c95ce6bbb4df8779ead
Simonesiss <simona.radulea@gmail.com>
2021-04-11 21:08:03 +0300

    Update rootReducer

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -2,8 +2,12 @@ import { combineReducers } from 'redux';
 
 import auth from './auth';
 import theme from './colorTheme'
+import events from './data/events'
+import goals from './data/goals'
 
 export default rootReducer = combineReducers({
     auth,
+    events,
+    goals,
     theme
 })
