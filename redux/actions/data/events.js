00459ca64399a7339365ffaff74c7a995bdca1d1
Simonesiss <simona.radulea@gmail.com>
2021-04-11 21:07:20 +0300

    Create events.js

---
+++ redux/actions/data/events.js
@@ -0,0 +1,34 @@
+import { firebase } from '../../../firebase/config';
+import * as types from './actionTypes';
+
+export const watchEventsData = (userId) => dispatch => {
+    dispatch(requestStart());
+    const reportsRef = firebase.firestore().collection('events').doc(userId).collection('sub_events');
+    reportsRef
+        .onSnapshot(querySnapshot => {
+            let eventsData = []
+            querySnapshot.forEach(doc => {
+                const event = doc.data()
+                event.id = doc.id
+                eventsData.push(event)
+            })
+            dispatch(requestSuccess(eventsData))
+        }),
+        (error) => {
+            dispatch(requestError(error))
+        }
+}
+
+const requestStart = () => ({
+    type: types.REQUEST_START
+});
+
+const requestSuccess = eventsData => ({
+    type: types.REQUEST_SUCCESS,
+    eventsData
+});
+
+const requestError = error => ({
+    type: types.REQUEST_ERROR,
+    error
+});
