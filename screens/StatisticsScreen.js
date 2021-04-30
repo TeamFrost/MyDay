4eb77a293648a05adc2deddedc25a494db4b7e98
Simonesiss <simona.radulea@gmail.com>
2021-04-30 20:28:33 +0300

    Add statistics data

--- screens/StatisticsScreen.js
+++ screens/StatisticsScreen.js
@@ -6,6 +6,9 @@ import { VictoryLine, VictoryChart, VictoryTheme, VictoryScatter, VictoryPie, Vi
 import { connect } from 'react-redux';
 import moment from 'moment';
 
+import { firebase } from "../firebase/config";
+import { occuarnce, compareTime, compareProgress } from '../helpers/functions'
+
 import HeaderGradient from '../assets/backgrounds/headerGradientBlue';
 import Back from '../assets/others/back.js';
 import ActivitiesIcon from "../assets/statistics/statisticsActivities";
@@ -19,45 +22,35 @@ const screenWidth = Dimensions.get('screen').width;
 const screenHeight = Dimensions.get('screen').height;
 const theme = colors.light;
 
-let DATA = [
-    { x: moment().subtract(6, 'days').format("DD/MM"), y: 2 },
-    { x: moment().subtract(5, 'days').format("DD/MM"), y: 3 },
-    { x: moment().subtract(4, 'days').format("DD/MM"), y: 7 },
-    { x: moment().subtract(3, 'days').format("DD/MM"), y: 5 },
-    { x: moment().subtract(2, 'days').format("DD/MM"), y: 6 },
-    { x: moment().subtract(1, 'days').format("DD/MM"), y: 11 },
-    { x: moment().format("DD/MM"), y: 8 },
-];
-
-const pieChartData = [
-    { x: "University", y: 21, color: "#6B75CE" },
-    { x: "Work", y: 29, color: "#564B93" },
-    { x: "Lifestyle", y: 18, color: "#9B8CF8" },
-    { x: "Sport", y: 25, color: "#D4C3FC" },
-    { x: "Shopping", y: 5, color: "#A5C5FC" },
-    { x: "Holiday", y: 8, color: "#5C8DF7" },
-];
-
 const mapStateToProps = (state) => ({
     user: state.auth.user,
+    events: state.events.eventsData,
     goals: state.goals.goalsData,
     theme: state.theme
 });
 
-
 function StatisticsScreen({ ...props }) {
 
-    const { user, navigation, goals } = props
-    console.log(goals)
+    const { user, navigation, events, goals } = props
 
-    const [activitiesNumber, setActivitiesNumber] = useState(1052)
-    const [activity, setActivity] = useState("University")
-    const [goalsNumber, setGoalsNumber] = useState(640)
-    const [goal, setGoal] = useState("Run 2k")
-    const [achievemnetsNumber, setAchievemnetsNumber] = useState(45)
-    const [achievement, setAchievement] = useState("Planner")
-    const [friendsNumber, setFriendsNumber] = useState(154)
-    const [friend, setFriend] = useState("Julie")
+    const [activitiesNumber, setActivitiesNumber] = useState(0)
+    const [activity, setActivity] = useState("")
+    const [goalsNumber, setGoalsNumber] = useState(0)
+    const [goal, setGoal] = useState("")
+    const [achievemnetsNumber, setAchievemnetsNumber] = useState(0)
+    const [achievement, setAchievement] = useState("")
+    const [friendsNumber, setFriendsNumber] = useState(0)
+    const [friend, setFriend] = useState("")
+    const [pieChartData, setPieChartData] = useState([])
+    const [lineChartData, setLineChartData] = useState([])
+
+    const sixDaysAgo = moment().subtract(6, 'days').format("DD/MM")
+    const fiveDaysAgo = moment().subtract(5, 'days').format("DD/MM")
+    const fourDaysAgo = moment().subtract(4, 'days').format("DD/MM")
+    const threeDaysAgo = moment().subtract(3, 'days').format("DD/MM")
+    const twoDaysAgo = moment().subtract(2, 'days').format("DD/MM")
+    const oneDayAgo = moment().subtract(1, 'days').format("DD/MM")
+    const today = moment().format("DD/MM")
 
     const ItemLegend = ({ color, title }) => (
         <View style={{ height: 40, alignItems: 'center', ...styles.flatListItem }}>
@@ -79,8 +72,113 @@ function StatisticsScreen({ ...props }) {
 
     useEffect(() => {
         LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
+
+        const eventsLength = events.length;
+        setActivitiesNumber(eventsLength)
+        if (eventsLength === 0) {
+            setActivity("Insufficient data")
+        } else {
+            const eventsCategoryArray = events.map(ev => ev.category)
+            const mostPopularActivity = occuarnce(eventsCategoryArray)
+            setActivity(mostPopularActivity)
+        }
+
+        const goalsLength = goals.length;
+        setGoalsNumber(goalsLength)
+        if (goalsLength === 0) {
+            setGoal("Insufficient data")
+        } else {
+            const completedGoals = goals.filter(gl => gl.completed === true)
+            const sortedGoals = completedGoals.sort(compareTime)
+            const goal = sortedGoals[goalsLength - 2];
+            setGoal(goal.title)
+        }
+
+        const achievements = props.route.params
+        const completedAchievements = achievements.filter(ach => ach.progress === 100)
+        const completedLengthAchievements = completedAchievements.length
+        setAchievemnetsNumber(completedLengthAchievements)
+        const sortedAchievements = achievements.sort(compareProgress);
+        const progressedAchievement = sortedAchievements[sortedAchievements.length - 2].achievement
+        setAchievement(progressedAchievement)
+
+        const friendsLength = user.friends.length
+        setFriendsNumber(friendsLength)
+        if (friendsLength === 0) {
+            setFriend("Insufficient data")
+        } else {
+            const friendsArray = events.map(ev => ev.friends)
+            const flattenFriendsArray = friendsArray.flat()
+            const friend = occuarnce(flattenFriendsArray)
+            const userRef = firebase.firestore().collection('users').doc(friend)
+            userRef.get()
+                .then(firestoreDocument => {
+                    if (!firestoreDocument.exists) {
+                        alert("User does not exist anymore.")
+                        return;
+                    }
+                    const user = firestoreDocument.data()
+                    setFriend(user.username)
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+        }
+
+        const uniEvents = events.filter(ev => ev.category === "University")
+        const uniEventsLength = uniEvents.length;
+        const workEvents = events.filter(ev => ev.category === "Work")
+        const workEventsLength = workEvents.length;
+        const lifestyleEvents = events.filter(ev => ev.category === "Lifestyle")
+        const lifestyleEventsLength = lifestyleEvents.length;
+        const sportEvents = events.filter(ev => ev.category === "Sport")
+        const sportEventsLength = sportEvents.length;
+        const shoppingEvents = events.filter(ev => ev.category === "Shopping")
+        const shoppingEventsLength = shoppingEvents.length;
+        const holidayEvents = events.filter(ev => ev.category === "Holiday")
+        const holidayEventsLength = holidayEvents.length;
+
+        const pieChart = [
+            { x: "University", y: uniEventsLength, color: "#6B75CE" },
+            { x: "Work", y: workEventsLength, color: "#564B93" },
+            { x: "Lifestyle", y: lifestyleEventsLength, color: "#9B8CF8" },
+            { x: "Sport", y: sportEventsLength, color: "#D4C3FC" },
+            { x: "Shopping", y: shoppingEventsLength, color: "#A5C5FC" },
+            { x: "Holiday", y: holidayEventsLength, color: "#5C8DF7" },
+        ]
+
+        setPieChartData(pieChart)
+
+        const activitiesSixDaysAgo = events.filter(ev => moment(new Date(ev.date)).format("DD/MM") === sixDaysAgo)
+        const activitiesSixDaysAgoLength = activitiesSixDaysAgo.length;
+        const activitiesFiveDaysAgo = events.filter(ev => moment(new Date(ev.date)).format("DD/MM") === fiveDaysAgo)
+        const activitiesFiveDaysAgoLength = activitiesFiveDaysAgo.length;
+        const activitiesFourDaysAgo = events.filter(ev => moment(new Date(ev.date)).format("DD/MM") === fourDaysAgo)
+        const activitiesFourDaysAgoLength = activitiesFourDaysAgo.length;
+        const activitiesThreeDaysAgo = events.filter(ev => moment(new Date(ev.date)).format("DD/MM") === threeDaysAgo)
+        const activitiesThreeDaysAgoLength = activitiesThreeDaysAgo.length;
+        const activitiesTwoDaysAgo = events.filter(ev => moment(new Date(ev.date)).format("DD/MM") === twoDaysAgo)
+        const activitiesTwoDaysAgoLength = activitiesTwoDaysAgo.length;
+        const activitiesOneDayAgo = events.filter(ev => moment(new Date(ev.date)).format("DD/MM") === oneDayAgo)
+        const activitiesOneDayAgoLength = activitiesOneDayAgo.length;
+        const activitiesToday = events.filter(ev => moment(new Date(ev.date)).format("DD/MM") === today)
+        const activitiesTodayLength = activitiesToday.length;
+
+        const lineChart = [
+            { x: sixDaysAgo, y: activitiesSixDaysAgoLength },
+            { x: fiveDaysAgo, y: activitiesFiveDaysAgoLength },
+            { x: fourDaysAgo, y: activitiesFourDaysAgoLength },
+            { x: threeDaysAgo, y: activitiesThreeDaysAgoLength },
+            { x: twoDaysAgo, y: activitiesTwoDaysAgoLength },
+            { x: oneDayAgo, y: activitiesOneDayAgoLength },
+            { x: today, y: activitiesTodayLength },
+        ];
+
+        setLineChartData(lineChart)
+
     }, [])
 
+    console.log(lineChartData)
     return (
         <View style={styles.container}>
             <HeaderGradient width={screenWidth * 1.2} height={"22%"} style={{ flex: 1, position: 'absolute' }} />
@@ -107,7 +205,7 @@ function StatisticsScreen({ ...props }) {
                         </View>
                         <View style={styles.belowView}>
                             <Star />
-                            <Text style={styles.belowText}>Most popular activity: <Text style={{ ...styles.belowText, fontWeight: "bold" }}>{activity}</Text></Text>
+                            <Text style={styles.belowText}>Most popular category: <Text style={{ ...styles.belowText, fontWeight: "bold" }}>{activity}</Text></Text>
                         </View>
                     </View>
 
@@ -119,7 +217,7 @@ function StatisticsScreen({ ...props }) {
                         </View>
                         <View style={styles.belowView}>
                             <Star />
-                            <Text style={styles.belowText}>Most recent goal: <Text style={{ ...styles.belowText, fontWeight: "bold" }}>{goal}</Text></Text>
+                            <Text style={styles.belowText}>Last completed goal: <Text style={{ ...styles.belowText, fontWeight: "bold" }}>{goal}</Text></Text>
                         </View>
                     </View>
 
@@ -133,7 +231,7 @@ function StatisticsScreen({ ...props }) {
                         </View>
                         <View style={styles.belowView}>
                             <Star />
-                            <Text style={styles.belowText}>Last achievement received <Text style={{ ...styles.belowText, fontWeight: "bold" }}>{achievement}</Text></Text>
+                            <Text style={styles.belowText}>Most progress towards: <Text style={{ ...styles.belowText, fontWeight: "bold" }}>{achievement}</Text></Text>
                         </View>
                     </View>
 
@@ -145,7 +243,7 @@ function StatisticsScreen({ ...props }) {
                         </View>
                         <View style={styles.belowView}>
                             <Star />
-                            <Text style={styles.belowText}>Last added friend: <Text style={{ ...styles.belowText, fontWeight: "bold" }}>{friend}</Text></Text>
+                            <Text style={styles.belowText}>Best friend: <Text style={{ ...styles.belowText, fontWeight: "bold" }}>{friend}</Text></Text>
                         </View>
                     </View>
                 </View>
@@ -165,10 +263,10 @@ function StatisticsScreen({ ...props }) {
                             }}
                             labels={({ datum }) => datum.y}
                             interpolation="monotoneX"
-                            data={DATA}
+                            data={lineChartData}
                         />
                         <VictoryScatter
-                            data={DATA}
+                            data={lineChartData}
                             style={{ data: { fill: theme.red } }}
                         />
                     </VictoryChart>
