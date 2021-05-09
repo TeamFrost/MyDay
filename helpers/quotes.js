c80f53817eb7396f2ac7610ea0de8258cce7b26c
Simonesiss <simona.radulea@gmail.com>
2021-05-09 14:32:17 +0300

    Create quotes.js

---
+++ helpers/quotes.js
@@ -0,0 +1,13 @@
+export const quotes = [
+    "We can do anything we want to if we stick to it long enough.", //0
+    "The secret of getting ahead is getting started.", //1
+    "The best time to plant a tree was 20 years ago. The second best time is now.", //2
+    "Write it. Shoot it. Publish it. Crochet it, sauté it, whatever. MAKE.", //3
+    "Do one thing every day that scares you.", //4
+    "One day or day one. You decide.", //5
+    "Invest in your dreams. Grind now. Shine later.", //6
+    "Keep your eyes on the stars, and your feet on the ground.", //7
+    "Work hard, be kind, and amazing things will happen.", //8
+    "Nothing will work unless you do.", //9
+    "In the middle of every difficulty lies opportunity.", //10
+]
