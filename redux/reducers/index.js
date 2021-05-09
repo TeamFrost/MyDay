7b6cf91c3b646970bde92736807351676fdafb7a
Simonesiss <simona.radulea@gmail.com>
2021-05-09 14:33:12 +0300

    Add notification reducer to store

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,13 +1,15 @@
 import { combineReducers } from 'redux';
 
 import auth from './auth';
-import theme from './colorTheme'
 import events from './data/events'
 import goals from './data/goals'
+import notifications from './data/notifications'
+import theme from './colorTheme'
 
 export default rootReducer = combineReducers({
     auth,
     events,
     goals,
+    notifications,
     theme
 })
