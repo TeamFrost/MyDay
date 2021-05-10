9010a131bda044c860c80c64706383b6e3505d52
Simonesiss <simona.radulea@gmail.com>
2021-05-10 18:27:16 +0300

    Modify SVG to accept color style

--- assets/icons/rightArrowIcon.js
+++ assets/icons/rightArrowIcon.js
@@ -13,7 +13,7 @@ export default (props) => {
         >
             <Path
                 d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"
-                fill="#323232"
+                fill="currentColor"
             />
         </Svg>
     )
