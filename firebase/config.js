cd7b55f4944d4f7b53ba8aadbe6333b43baa98de
SimonaRadulea <48724811+SimonaRadulea@users.noreply.github.com>
2021-06-29 17:43:19 +0300

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_J54qsCPPmyyeNK_OQJBTKrxDGEpbdyg",
-    authDomain: 'myday-b93b4.firebaseapp.com',
-    databaseURL: 'https://myday-b93b4.firebaseio.com',
-    projectId: 'myday-b93b4',
-    storageBucket: 'myday-b93b4.appspot.com',
-    messagingSenderId: '26219097559',
-    appId: '1:26219097559:web:934c1d08cdd2e330ddef92',
+    apiKey: "***",
+    authDomain: '***',
+    databaseURL: '***',
+    projectId: '***',
+    storageBucket: '***',
+    messagingSenderId: '***',
+    appId: '***',
 };
 
 if (!firebase.apps.length) {
